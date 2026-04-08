6d968507da5d44a895d4c61f95e2908557393339
Kari Hoglund Kounkel <kari@karikounkel.com>
2026-04-07 21:43:25 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,3 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        priority: resolve(__dirname, "priority.html"),
-      },
-    },
-  },
-});
+export default defineConfig({ plugins: [react()] });
